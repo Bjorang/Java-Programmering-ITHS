f7254b4520c01f7f13034f89e3ba0d0710215ada
Bjorang <anders.bjorang@iths.se>
2025-09-15 14:42:22 +0200

    Döpt om HelloWorld till HelloWorldAB

---
+++ HelloWorld2.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Hej Världen");
+        System.out.println();
+    }
+}
---
+++ HelloWorldAB.java
@@ -0,0 +1,5 @@
+public class HelloWorldAB {
+    
+public static void main(String[] args) {
+    System.out.println("Hello World")
+}}
