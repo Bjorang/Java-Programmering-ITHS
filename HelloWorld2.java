0f62f10f4aa2140f9d2315b47958d4bc2031f789
Bjorang <anders.bjorang@iths.se>
2025-09-17 11:20:12 +0200

    deleted

--- HelloWorld2.java
+++
@@ -1,8 +0,0 @@
-
-public class HelloWorld2 {
-    public static void main(String[] args) {
-        System.out.println();
-        System.out.println("Hej Världen är du bra");
-        System.out.println();
-    }
-}
