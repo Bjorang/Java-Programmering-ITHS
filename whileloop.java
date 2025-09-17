f70262176578fa076e6820079c9c7bf84289fa0c
Bjorang <anders.bjorang@iths.se>
2025-09-17 10:54:12 +0200

    SkrivNamnövn

---
+++ whileloop.java
@@ -0,0 +1,16 @@
+public class whileloop {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i>0) {
+            System.out.println(i);
+            i++;
+          if (i > 10){break;}  
+        }
+    
+        
+        for (int j=1; j<10;j++) {System.out.println(j + "slut");}
+
+    }
+    
+}
+
