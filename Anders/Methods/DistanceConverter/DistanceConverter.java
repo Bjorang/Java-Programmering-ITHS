861c52ddd2f274e5cd487302b92306762763f831
Bjorang <anders.bjorang@iths.se>
2025-09-25 08:28:09 +0000

    Closed a Scanner

--- Anders/Methods/DistanceConverter/DistanceConverter.java
+++ Anders/Methods/DistanceConverter/DistanceConverter.java
@@ -7,13 +7,21 @@ public class DistanceConverter {
         double distance =scan.nextDouble();
 
         milesToKilometer(distance);
+       scan.close(); 
     }
+    
     static void milesToKilometer(double miles){
         double kilometers = miles * 1.60934;
         System.out.println(kilometers);
+        
     }
     static void kilometersToMiles(double kilometers){
         double miles=kilometers / 1.60934;
         System.out.println(miles);
+        
+
+        
     }
+    
 }
+
