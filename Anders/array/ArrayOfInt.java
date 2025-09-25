dcf16ed6d0ed037102fc941b6b563badfb64872f
Bjorang <anders.bjorang@iths.se>
2025-09-25 19:57:06 +0000

    Gjorde en T100

--- Anders/array/ArrayOfInt.java
+++ Anders/array/ArrayOfInt.java
@@ -1,19 +1,29 @@
 package Anders.array;
-import java.util.Random; 
+
+import java.util.Random;
 
 public class ArrayOfInt {
-    
-    int[] javaInt = {1,2,3,4,5,6,7,8,9,10};
+    public static void main(String[] args) {
 
-
-    public void randomArray (){ ;
-        Random rand = new Random();
-
-        
-        
-        int randomindex = rand.nextInt(javaInt.length);
-        int randomNr =javaInt[randomindex];
-        System.out.println("slumpa ett tal "+ randomNr);
-    
+        randomArray();
     }
+
+    static int[] randomInt;
+
+    public static void randomArray() {
+        randomInt = new int[100];
+        Random rand = new Random();
+        for (int i = 0; i < randomInt.length; i++)
+            randomInt[i] = rand.nextInt(100) + 1;
+        int randomindex = rand.nextInt(randomInt.length);
+        int randomNr = randomInt[randomindex];
+
+        int randomIndex2 = rand.nextInt(randomInt.length);
+        int randomNr2 = randomInt[randomIndex2];
+
+        System.out.println("Första slumpade talet: " + randomNr);
+        System.out.println("Andra slumpade talet: " + randomNr2);
+
+    }
+
 }
