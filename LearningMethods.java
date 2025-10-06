40725b9d807077f5e49eb510b9d6afc25113c1e2
Bjorang <anders.bjorang@iths.se>
2025-10-06 11:28:41 +0200

    Bröt ut från main

--- LearningMethods.java
+++ LearningMethods.java
@@ -2,39 +2,51 @@ import java.util.Scanner;
 
 public class LearningMethods {
     public static void main(String[] args) {
-        String name;
-        int age;
-        boolean indoorPerson = false;
-        boolean outdoorPerson = false;
+
+        new LearningMethods().welcomeMessage();
+        new LearningMethods().getUserInfo();
+        new LearningMethods().indoorOrOutdoor();
+    }
+
+    public static boolean indoorPerson = false;
+    public static boolean outdoorPerson = false;
+    public static Scanner sc = new Scanner(System.in);
+
+    public void welcomeMessage() {
 
         System.out.println("Hello! I want to show you how to use methods!");
+
+    }
+
+    public void getUserInfo() {
         System.out.println("First off, what is your name?");
-        Scanner sc = new Scanner (System.in);
-        name = sc.nextLine();
+        String name = sc.nextLine();
         System.out.println("Nice to meet you " + name + "!");
         System.out.println("What is your age?");
-        age = sc.nextInt();
+        int age = sc.nextInt();
         System.out.println("That means you where born in " + (2025 - age) + "!");
+    }
+
+    public void indoorOrOutdoor() {
         System.out.println("Are you an indoor or outdoor person?");
         sc.nextLine();
         String answer = sc.nextLine();
-        if(answer.matches("indoor")|| answer.matches("Indoor")) {
+        if (answer.matches("indoor") || answer.matches("Indoor")) {
             indoorPerson = true;
-        }
-        else if(answer.matches("outdoor") || answer.matches("Outdoor")){
+        } else if (answer.matches("outdoor") || answer.matches("Outdoor")) {
             outdoorPerson = true;
-        }
-        else{
+        } else {
             System.out.println("That was an unexpected answer! O.o");
         }
-        if(indoorPerson == true){
+        if (indoorPerson == true) {
             System.out.println("Awesome! What is it that you do indoors?");
-        }else if (outdoorPerson == true) {
+        } else if (outdoorPerson == true) {
             System.out.println("Wow cool! Thats unusual nowadays. What is it you like about the great outdoors?");
-        }else{
+        } else {
             System.out.println("Lets start over!");
         }
 
         sc.close();
+
     }
 }
