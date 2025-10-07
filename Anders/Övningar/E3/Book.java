7fd8beff1778628f8e8dd7d393ceb9291dc979fb
Bjorang <anders.bjorang@iths.se>
2025-10-07 14:26:46 +0200

    Bok

---
+++ Anders/Övningar/E3/Book.java
@@ -0,0 +1,27 @@
+package Anders.Övningar.E3;
+
+public class Book {
+
+    String title;
+         String author;
+         int year;
+
+    public static void main(String[] args) {
+     Book book1 = new Book ();
+     
+
+     System.out.println(book1.title);   
+     System.out.println(book1.author);
+     System.out.println(book1.year); x
+
+    }   
+
+        
+    
+    public Book(){
+      this.title = "okänd titel";
+      this.author ="okänd författare";
+      this.year = 0; 
+    
+    }
+}
